cf4ebf5e970908d6f37dd4e490954499e98c9ffd
Rohan Shrestha <rohan.org.com@gmail.com>
2026-02-25 18:24:59 +0545

    class and obj program

---
+++ Test.java
@@ -0,0 +1,21 @@
+ package LocalRepo;
+class Demo{
+
+
+    int a = 10;
+    String b = "rohan";
+
+    void show(){
+        System.out.println(a + " " +b);
+    }
+}
+
+
+public class   Test {
+    public static void main(String[] args) {
+        Demo r = new Demo();
+        r.show();
+    }
+
+    
+}
